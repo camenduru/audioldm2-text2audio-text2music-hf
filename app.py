8f84458bfcd15717f0e4fa18be035f1fd43fc597
haoheliu <867390095@qq.com>
2023-08-04 18:11:35 +0100

    first commit

--- app.py
+++ app.py
@@ -357,5 +357,5 @@ with iface:
 # <p>This demo is strictly for research demo purpose only. For commercial use please <a href="haoheliu@gmail.com">contact us</a>.</p>
 
 iface.queue(concurrency_count=3)
-# iface.launch(debug=True)
-iface.launch(debug=True, share=True)
+iface.launch(debug=True)
+# iface.launch(debug=True, share=True)
